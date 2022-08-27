774803864a52e43f86e1f5d8c919cc17ff79c53d
itachi2003 <lomsaadze.lazare2003@gmail.com>
2022-08-27 13:12:03 +0400

    erc721a-extra-function

---
+++ config/CollectionConfig.ts
@@ -0,0 +1,35 @@
+import CollectionConfigInterface from '../lib/CollectionConfigInterface';
+import * as Networks from '../lib/Networks';
+import * as Marketplaces from '../lib/Marketplaces';
+import whitelistAddresses from './whitelist.json';
+
+const CollectionConfig: CollectionConfigInterface = {
+  testnet: Networks.ethereumTestnet,
+  mainnet: Networks.ethereumMainnet,
+  // The contract name can be updated using the following command:
+  // yarn rename-contract NEW_CONTRACT_NAME
+  // Please DO NOT change it manually!
+  contractName: 'IP',
+  tokenName: 'Smart Contract',
+  tokenSymbol: 'SC',
+  hiddenMetadataUri: 'ipfs://UPDATE/hidden.json',
+  maxSupply: 1111,
+  whitelistSale: {
+    price: 0.00001,
+    maxMintAmountPerTx: 89,
+  },
+  preSale: {
+    price: 0.07,
+    maxMintAmountPerTx: 2,
+  },
+  publicSale: {
+    price: 0,
+    maxMintAmountPerTx: 1,
+  },
+  contractAddress: null,
+  marketplaceIdentifier: 'my-nft-token',
+  marketplaceConfig: Marketplaces.openSea,
+  whitelistAddresses,
+};
+
+export default CollectionConfig;
---
+++ lib/Networks.ts
@@ -0,0 +1,60 @@
+import NetworkConfigInterface from './NetworkConfigInterface';
+
+/*
+ * Local networks
+ */
+export const hardhatLocal: NetworkConfigInterface = {
+  chainId: 31337,
+  symbol: 'ETH (test)',
+  blockExplorer: {
+    name: 'Block explorer (not available for local chains)',
+    generateContractUrl: (contractAddress: string) => `#`,
+    generateTransactionUrl: (transactionAddress: string) => `#`,
+  },
+}
+
+/*
+ * Ethereum
+ */
+export const ethereumTestnet: NetworkConfigInterface = {
+  chainId: 4,
+  symbol: 'ETH (test)',
+  blockExplorer: {
+    name: 'Etherscan (Rinkeby)',
+    generateContractUrl: (contractAddress: string) => `https://rinkeby.etherscan.io/address/${contractAddress}`,
+    generateTransactionUrl: (transactionAddress: string) => `https://rinkeby.etherscan.io/tx/${transactionAddress}`,
+  },
+}
+
+export const ethereumMainnet: NetworkConfigInterface = {
+  chainId: 1,
+  symbol: 'ETH',
+  blockExplorer: {
+    name: 'Etherscan',
+    generateContractUrl: (contractAddress: string) => `https://etherscan.io/address/${contractAddress}`,
+    generateTransactionUrl: (transactionAddress: string) => `https://etherscan.io/tx/${transactionAddress}`,
+  },
+}
+
+/*
+ * Polygon
+ */
+export const polygonTestnet: NetworkConfigInterface = {
+  chainId: 80001,
+  symbol: 'MATIC (test)',
+  blockExplorer: {
+    name: 'Polygonscan (Mumbai)',
+    generateContractUrl: (contractAddress: string) => `https://mumbai.polygonscan.com/address/${contractAddress}`,
+    generateTransactionUrl: (transactionAddress: string) => `https://mumbai.polygonscan.com/tx/${transactionAddress}`,
+  },
+}
+
+export const polygonMainnet: NetworkConfigInterface = {
+  chainId: 137,
+  symbol: 'MATIC',
+  blockExplorer: {
+    name: 'Polygonscan',
+    generateContractUrl: (contractAddress: string) => `https://polygonscan.com/address/${contractAddress}`,
+    generateTransactionUrl: (transactionAddress: string) => `https://polygonscan.com/tx/${transactionAddress}`,
+  },
+}
